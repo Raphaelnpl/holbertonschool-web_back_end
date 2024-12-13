621bf25027382fce487cf45e5e7e5a46cb948e97
Raphael Melnique <158978700+Raphaelnpl@users.noreply.github.com>
2024-12-13 14:23:33 +0000

    add miss

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,9 +4,11 @@ A coroutine that collects random numbers from an async generator using
 async comprehension.
 """
 
-import asyncio
+import importlib
 from typing import List
-from 0-async_generator import async_generator
+
+# Dynamically import the module
+async_generator = importlib.import_module('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """
